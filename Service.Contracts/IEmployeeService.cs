8d9975e0a8d8f3b07f6430ab04dcaf4fd1f447d6
christian.kinate <christian.kinate@FINTRAKSOFWARE.COM>
2024-10-23 19:38:14 +0100

    FirstCommit

---
+++ Service.Contracts/IEmployeeService.cs
@@ -0,0 +1,18 @@
+﻿using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Contracts
+{
+    public interface IEmployeeService
+    {
+
+        IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
+        EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges);
+
+
+    }
+}
