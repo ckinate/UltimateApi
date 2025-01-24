d6096565fd006127de88683f91cc0c71ed43c847
Christian Kinate <kinate.christian1989@yahoo.com>
2025-01-24 14:21:18 -0800

    AuthenUsersAndRoles

--- CompanyEmployees/obj/Debug/net6.0/CompanyEmployees.AssemblyInfo.cs
+++ CompanyEmployees/obj/Debug/net6.0/CompanyEmployees.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e607aea1644f3bb489fd70422ed8a0c0180371")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb3a4471706e05ab3935bd111f4e4eaffb28aad1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Service/AuthenticationService .cs
+++ Service/AuthenticationService .cs
@@ -4,11 +4,14 @@ using Entities.Exceptions;
 using Entities.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +24,7 @@ namespace Service
         private readonly UserManager<User> _userManager;
         private readonly IConfiguration _configuration;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private User? _user;
 
         public AuthenticationService(ILoggerManager logger, IMapper mapper,UserManager<User> userManager, IConfiguration configuration, RoleManager<IdentityRole> roleManager)
         {
@@ -56,6 +60,54 @@ namespace Service
             }
                
             return result;
+        }
+
+        public async Task<bool> ValidateUser(UserForAuthenticationDto userForAuth)
+        {
+            _user = await _userManager.FindByNameAsync(userForAuth.UserName);
+
+            var result = (_user != null && await _userManager.CheckPasswordAsync(_user, userForAuth.Password));
+            if (!result) _logger.LogWarn($"{nameof(ValidateUser)}: Authentication failed. Wrong user name or password.");
+
+            return result;
+        }
+
+        public async Task<string> CreateToken()
+        {
+            var signingCredentials = GetSigningCredentials(); 
+            var claims = await GetClaims(); 
+            var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        }
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SECRET")); 
+            var secret = new SymmetricSecurityKey(key);
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, _user.UserName) };
+
+            var roles = await _userManager.GetRolesAsync(_user); 
+            foreach (var role in roles)
+            { 
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+
+            var tokenOptions = new JwtSecurityToken(issuer: jwtSettings["validIssuer"], audience: jwtSettings["validAudience"], claims: claims, expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["expires"])), signingCredentials: signingCredentials);
+
+            return tokenOptions;
         }
 
 
